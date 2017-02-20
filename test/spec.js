ba04b854a36de80a32b05a92b409c78a8ab098e3
Jeremy Sande <sandejeremy@gmail.com>
2017-02-20 15:41:47 -0800

    add tests

--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,56 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+var db = require('../db/db-controller');
+
 describe('An Orc in The Road', function() {
 
-	it('should', function() {
-		expect(true).to.equal(true);
-	});
-	
+	describe('Database - MySQL', function() {
+
+		describe('Quests', function() {
+
+			it('should add fetch quests to db', function(done) {
+				var quest = {name: 'testQuest2', type: 'addFetchQuest', creator_id: 1, experience: 100, lat: 100, lng: 100};
+				db.addFetchQuest(quest).then(function() {
+					db.connection.queryAsync('SELECT name FROM Quests WHERE name = "testQuest"').then(function(result) {
+						expect(result[result.length - 1].name).to.exist;
+						done();
+					});
+				});
+			});
+
+			it('should get all quests', function(done) {
+				var quest = {name: 'testQuest3', type: 'addFetchQuest', creator_id: 1, experience: 100, lat: 100, lng: 100};
+				db.addFetchQuest(quest).then(function() {
+					db.getAllQuests().then(function(result) {
+						expect(!!result[result.length - 1].name).to.equal(true);
+						done();
+					});
+				});
+			});
+
+			it('should complete a quest by id', function(done) {
+				db.completeQuest(1, 1).then(function(result) {
+					db.connection.queryAsync('SELECT complete FROM Quests WHERE id = 1').then(function(result) {
+						expect(!!result[0].complete).to.equal(true);
+						done()
+					})
+				});
+			});
+
+		});
+
+		describe('Characters', function() {
+			
+			it('should get a character by id', function(done) {
+				db.getCharacter(1).then(function(result) {
+					expect(result[0].name).to.equal('Aldric Testmane');
+					done();
+				});
+			});
+
+		});
+
+
+	});	
 });
