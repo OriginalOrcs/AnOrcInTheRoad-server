ec706d7d6f5fcd0f07bb472ba073c826aed61dcf
Andrew Hsu <andrewhsu42@gmail.com>
2017-02-24 21:51:10 -0800

    Refactor to integrate with client-side code

--- db/db-controller.js
+++ db/db-controller.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   host: process.env.db || "localhost",
   database: "Orc",
   user: "root",
-  password: "originalorcs"
+  password: ""
 });
 
 connection = Promise.promisifyAll(connection);
@@ -19,15 +19,18 @@ connection.connect();
 // add quest router.
 var addQuest = function(quest) {
 	var that = this;
+  console.log('addquest quest', quest);
+  console.log(quest.questType);
+  console.log(that[quest.questType]);
 	return new Promise((resolve, reject) => {
-		return that[quest.type](quest).then(resolve);
+		return that[quest.questType](quest).then(resolve);
 	});
 }
 
 // adds a fetch quest to the db
 var addFetchQuest = function(quest) {
 	return new Promise(function(resolve, reject) {
-		var bufferQuest = { name: quest.name, creator_id: quest.creator_id, experience: quest.experience, lat: quest.lat, lng: quest.lng, type: quest.type}
+		var bufferQuest = { name: quest.name, creator_id: quest.creator_id, experience: quest.experience, lat: quest.lat, lng: quest.lng, type: quest.questType}
 		return connection.queryAsync('INSERT INTO Quests SET ?', bufferQuest).then(resolve);
 	});
 }
