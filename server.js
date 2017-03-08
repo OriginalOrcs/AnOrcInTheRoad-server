b5656b3d9f8e89b63eec8c024a770d19b0a44927
Jeremy Sande <sandejeremy@gmail.com>
2017-03-08 15:50:01 -0800

    bug fix mysql getAllQuests

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var socketHandler = require('./socket-handler').socketHandler;
-var leaveParty = require('./socket-handler').leaveParty;
 
 var port = process.env.port || 3000
 server.listen(port);
@@ -11,6 +10,24 @@ console.log('Listening on port: ' + port);
 var sockets = {};
 
 var users = {};
+
+var parties = {};
+
+
+var leaveParty = function(characterId) {
+	parties[characterId].characters.forEach(function(character, i) {
+		if (character.id === characterId) {
+			var sockets = parties[characterId].sockets.slice();
+			parties[characterId].characters.splice(i, 1);
+			parties[characterId].sockets.splice(i, 1);
+			parties[characterId] = undefined;
+			parties[characterId].characters.forEach(function(character, i) {
+				parties[character.id].socket[i].emit('update party', parties[character.id].characters);
+			});
+		}
+	});
+}
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -22,8 +39,10 @@ io.on('connection', function(socket) {
 	socket.on('disconnect', function() {
 		console.log('User Disconnected ' + sockets.length + ' Socket(s) Upheld')
 		delete users[sockets[socket.id]];
-		leaveParty(sockets[socket.id]);
+		if (parties[sockets[socket.id]]) {
+			leaveParty(sockets[socket.id]);
+		}
 	});
 
-	socketHandler(socket, io, sockets, users);
+	socketHandler(socket, io, sockets, users, parties, leaveParty);
 });
