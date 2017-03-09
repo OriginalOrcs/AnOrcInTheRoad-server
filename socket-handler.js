011e522bab81339a60ea454efa898e8ce0210ad4
Jeremy Sande <sandejeremy@gmail.com>
2017-03-08 16:57:33 -0800

    reject user does not exist

--- socket-handler.js
+++ socket-handler.js
@@ -13,17 +13,21 @@ var socketHandler = function(socket, io, sockets, users, parties, leaveParty) {
 	socket.on('add to party', function(characterId, targetName) {
 		db.getCharacterByName(targetName).then(function(target) {
 			target = target[0];
-			if (users[target.id]) {
-				if (!parties[target.id]) {
-					db.getCharacter(characterId).then(function(inviter) {
-						inviter = inviter[0];
-						users[target.id].emit('party invite', {inviter: inviter, invitee: target});
-					});
+			if (target)	{
+				if (users[target.id]) {
+					if (!parties[target.id]) {
+						db.getCharacter(characterId).then(function(inviter) {
+							inviter = inviter[0];
+							users[target.id].emit('party invite', {inviter: inviter, invitee: target});
+						});
+					} else {
+						socket.emit('reject user already in party');
+					}
 				} else {
-					socket.emit('reject user already in party');
+					socket.emit('reject user not online');
 				}
 			} else {
-				socket.emit('reject user not online');
+				socket.emit('reject user does not exist');
 			}
 		});
 	});
